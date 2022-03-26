bcf42b6434279bfe967f8baea0ce6aa81c865681
Louis Abraham <louis.abraham@yahoo.fr>
2022-03-26 23:32:09 +0100

    v0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="fastnode2vec",
-    version="0.0.5",
+    version="0.0.6",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
